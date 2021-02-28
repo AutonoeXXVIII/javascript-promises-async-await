a4eaee23ef31f2e63329f295e670828834bc1b37
AutonoeXXVIII <AutonoeXXVIII@users.noreply.github.com>
2021-02-28 21:55:59 +0800

    Complete module7

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ getBooksOrMoviesPromise.then(results => {
 
 async function getBooksAndMoviesAsync() {
   try {
-    const [books, movies] = await Promise.all([asyncFetchBooks, asyncFetchMovies]);
+    const [books, movies] = await Promise.all([asyncFetchBooks(), asyncFetchMovies()]);
     return { books, movies };
   } catch (error) {
     console.log("Error fetching books and movies", error);
@@ -38,10 +38,10 @@ async function getBooksAndMoviesAsync() {
 
 async function getBooksOrMoviesAsync() {
   try {
-    const values = await Promise.race([asyncFetchBooks, asyncFetchMovies]);
+    const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
     return values;
   } catch (error) {
-    console.log("Error waiting for the promise race", error);
+    console.error("Error waiting for the promise race", error);
   }
 }
 
